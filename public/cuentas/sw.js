e337fc4b0550d81379cdcddfbb924e4a447eb26b
Jon Arsuaga <jon@proyektaviajes.es>
2026-08-20 15:36:47 +0200

    Fix iPad unlock and sync invoice attachments

--- public/cuentas/sw.js
+++ public/cuentas/sw.js
@@ -1,4 +1,4 @@
-const CACHE='proyekta-cuentas-v2';
+const CACHE='proyekta-cuentas-v3';
 const ASSETS=['/cuentas/index.html','/cuentas/styles.css','/cuentas/app.js','/cuentas/manifest.webmanifest','/cuentas/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
